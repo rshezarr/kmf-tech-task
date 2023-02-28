c77410738ede68d6ab4a34adfc6b5ee510d1e37e
rshezarr <rshezarr@gmail.ru>
2023-02-28 10:02:44 +0600

    add: handler entity

---
+++ handler/handler.go
@@ -0,0 +1,32 @@
+package handler
+
+import (
+	"sync"
+)
+
+type ProxyRequest struct {
+	Method  string            `json:"method"`
+	URL     string            `json:"url"`
+	Headers map[string]string `json:"headers"`
+}
+
+type ProxyResponse struct {
+	ID      string            `json:"id"`
+	Status  int               `json:"status"`
+	Headers map[string]string `json:"headers"`
+	Lenght  int               `json:"length"`
+}
+
+type Handler struct {
+	Requests  map[string]ProxyRequest
+	Responses map[string]ProxyRequest
+	mu        sync.RWMutex
+}
+
+func NewHandler() *Handler {
+	return &Handler{
+		Requests:  make(map[string]ProxyRequest),
+		Responses: make(map[string]ProxyRequest),
+		mu:        sync.RWMutex{},
+	}
+}
