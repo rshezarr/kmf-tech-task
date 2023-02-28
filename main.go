a97a5d2d272bcea30dec32658a66e9d5baf5d236
rshezarr <rshezarr@gmail.ru>
2023-02-28 10:35:45 +0600

    update: main

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
-import "proxy-server/handler"
+import (
+	"fmt"
+	"proxy-server/handler"
+)
 
 func main() {
 	handler := handler.NewHandler()
 
+	fmt.Printf("proxy server started -> http://localhost:8080")
 	handler.InitRoutes()
 }
