fcccf843cb72e37279f61f2c8e8d1922d2d33aa2
rshezarr <rshezarr@gmail.ru>
2023-02-28 09:50:15 +0600

    init: request entity

--- server/server.go
+++ server/server.go
@@ -1,1 +1,10 @@
 package server
+
+type ProxyRequest struct {
+	Method  string            `json:"method"`
+	URL     string            `json:"url"`
+	Headers map[string]string `json:"headers"`
+}
+
+type ProxyResponse struct {
+}
